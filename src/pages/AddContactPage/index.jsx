8a0810b8f8e3d1fa5a66d0404ea2e01dcb0b0bda
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-07-28 10:20:49 -0300

    feat: add contacts form

--- src/pages/AddContactPage/index.jsx
+++ src/pages/AddContactPage/index.jsx
@@ -2,18 +2,32 @@ import { useState } from "react";
 import { AddContactContainer } from "./styled";
 
 export default function AddContactPage() {
-    const { form, setForm } = useState({ name: "", email: "", password: "", confirmPassword: "" })
+    const [name, setName] = useState("");
+    const [contact, setContact] = useState("");
+    const [email, setEmail] = useState("");
+    const [picture, setPicture] = useState("");
 
-    function handleForm(e) {
-        setForm({ ...form, [e.target.name]: e.target.value })
-    }
-    function submitForm(e) {
-        e.preventDefault()
+    function submitContact() {
+        const contacts = JSON.parse(localStorage.getItem("contacts")) || [];
+        const newContact = {
+            name,
+            contact,
+            email,
+            picture,
+            id: contacts.length + 1
+        };
+
+        const updatedContacts = [...contacts, newContact];
+        localStorage.setItem("contacts", JSON.stringify(updatedContacts));
     }
 
     return (
         <AddContactContainer>
-            <input type="button" value="Add Contact" />
+            <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} value={name} />
+            <input type="text" placeholder="Contact Number" onChange={(e) => setContact(e.target.value)} value={contact} />
+            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} />
+            <input type="text" placeholder="Picture" onChange={(e) => setPicture(e.target.value)} value={picture} />
+            <button onClick={submitContact}>Submit</button>
         </AddContactContainer>
-    )
+    );
 }
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,10 +4,14 @@ import { AddContactButton, HomePageContainer } from "./styled";
 export default function HomePage() {
 
     const navigate = useNavigate();
-    
+
+    const contacts = JSON.parse(localStorage.getItem("contacts"))
+    console.log(contacts)
+
     return (
         <HomePageContainer>
             <h1>Contacts</h1>
+            
             <AddContactButton onClick={() => navigate("/add")}>+</AddContactButton>
         </HomePageContainer>
     )
