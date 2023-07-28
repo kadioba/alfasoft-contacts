e5636497b253797ec3d9e4e09d3e0766dc134096
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-07-28 15:26:21 -0300

    fix: unique email and contact

--- src/pages/AddContactPage/index.jsx
+++ src/pages/AddContactPage/index.jsx
@@ -13,23 +13,30 @@ export default function AddContactPage() {
 
     useEffect(() => {
         if (id) {
-            const findContacts = JSON.parse(localStorage.getItem("contacts")) || [];
-            const contactFound = findContacts.find(contactFound => contactFound.id === Number(id));
-            if (!contactFound) {
+            const findContacts = JSON.parse(localStorage.getItem("contacts"));
+            if (findContacts) {
+                const contactFound = findContacts.find(contactFound => contactFound.id === Number(id));
+                if (contactFound) {
+                    setName(contactFound.name);
+                    setContact(contactFound.contact);
+                    setEmail(contactFound.email);
+                    setPicture(contactFound.picture);
+                } else {
+                    alert("Contact not found");
+                    navigate("/");
+                }
+            } else {
                 alert("Contact not found");
                 navigate("/");
-            } else {
-                setName(contactFound.name);
-                setContact(contactFound.contact);
-                setEmail(contactFound.email);
-                setPicture(contactFound.picture);
             }
+
         }
     }, [id, navigate]);
 
     function submitContact(e) {
         e.preventDefault();
-        if (verifyContactAndEmail()) return;
+        const duplicatedData = verifyContactAndEmail();
+        if (duplicatedData) return
         const contacts = JSON.parse(localStorage.getItem("contacts")) || [];
         const newContact = {
             name,
@@ -38,6 +45,7 @@ export default function AddContactPage() {
             picture,
             id: contacts.length + 1
         };
+
         const updatedContacts = [...contacts, newContact];
         localStorage.setItem("contacts", JSON.stringify(updatedContacts));
         navigate("/");
@@ -45,40 +53,42 @@ export default function AddContactPage() {
 
     function updateContact(e) {
         e.preventDefault();
-        if (verifyContactAndEmail()) return;
+        const duplicatedData = verifyContactAndEmail();
+        if (duplicatedData) return
         const contacts = JSON.parse(localStorage.getItem("contacts")) || [];
         const contactIndex = contacts.findIndex(contact => contact.id === Number(id));
-        if (contactIndex === -1) {
-            alert("Contact not found");
-            navigate("/");
-        } else {
-            contacts[contactIndex].name = name;
-            contacts[contactIndex].contact = contact;
-            contacts[contactIndex].email = email;
-            contacts[contactIndex].picture = picture;
-            localStorage.setItem("contacts", JSON.stringify(contacts));
-            navigate("/");
-        }
+        contacts[contactIndex].name = name;
+        contacts[contactIndex].contact = contact;
+        contacts[contactIndex].email = email;
+        contacts[contactIndex].picture = picture;
+        localStorage.setItem("contacts", JSON.stringify(contacts));
+        navigate("/");
     }
 
     function verifyContactAndEmail() {
         const contacts = JSON.parse(localStorage.getItem("contacts")) || [];
-        const contactFind = contacts.find(contact => contact.contact === contact);
-        const emailFind = contacts.find(contact => contact.email === email);
+        const contactFind = contacts.find(contactFound => contactFound.contact === contact);
+        const emailFind = contacts.find(contactFound => contactFound.email === email);
         if (contactFind) {
+            if (id && (contactFind.id === Number(id))) {
+                return false
+            }
             if (id && (contactFind.id !== Number(id))) {
-                alert("Contact already exists");
-                return true;
+                alert("Contact already exists 1");
+                return true
             } else {
-                alert("Contact already exists");
+                alert("Contact already exists 2");
                 return true;
             }
         } else if (emailFind) {
+            if (id && (emailFind.id === Number(id))) {
+                return false
+            }
             if (id && (emailFind.id !== Number(id))) {
-                alert("Email already exists");
+                alert("Email already exists 1");
                 return true;
             } else {
-                alert("Email already exists");
+                alert("Email already exists 2");
                 return true;
             }
         }
@@ -89,34 +99,10 @@ export default function AddContactPage() {
         <AddContactContainer>
             <h1>{id ? "Edit Contact" : "Add Contact"}</h1>
             <form onSubmit={id ? updateContact : submitContact}>
-                <input 
-                    type="text" 
-                    placeholder="Name" 
-                    onChange={(e) => setName(e.target.value)} 
-                    value={name} 
-                    minLength={5} 
-                />
-                <input 
-                    type="tel" 
-                    placeholder="Contact Number" 
-                    onChange={(e) => setContact(e.target.value)} 
-                    value={contact} 
-                    minLength={9} 
-                    maxLength={9} 
-                />
-                <input 
-                    type="email" 
-                    placeholder="Email" 
-                    onChange={(e) => setEmail(e.target.value)} 
-                    value={email} 
-                />
-                <input 
-                    type="text" 
-                    placeholder="Picture" 
-                    onChange={(e) => setPicture(e.target.value)} 
-                    value={picture} 
-                    required 
-                />
+                <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} value={name} minLength={5} />
+                <input type="tel" placeholder="Contact Number" onChange={(e) => setContact(e.target.value)} value={contact} minLength={9} maxLength={9} />
+                <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} />
+                <input type="text" placeholder="Picture" onChange={(e) => setPicture(e.target.value)} value={picture} required />
                 <button type="submit">Submit</button>
             </form>
         </AddContactContainer>
